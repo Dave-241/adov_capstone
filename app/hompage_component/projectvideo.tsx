3a7144ea63e2265988501b07df153120504eecc8
dera <animicks.123.4@gmail.com>
2026-08-10 09:40:43 +0100

    update

--- app/hompage_component/projectvideo.tsx
+++ app/hompage_component/projectvideo.tsx
@@ -1,7 +1,7 @@
 export default function StudySession() {
   return (
-    <div className=" mx-auto md:pt-16 pt-4 bg-[#fffff0]">
-      <div className="relative w-full aspect-square md:aspect-[16/10] overflow-hidden">
+    <div className=" mx-auto md:pt-10 md:pb-5 pt-4 bg-[#fffff0]">
+      <div className="relative w-full aspect-square md:aspect-[16/7] overflow-hidden">
         <video
           src="/session.mp4"
           autoPlay
