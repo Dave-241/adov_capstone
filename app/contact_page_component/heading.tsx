1166a357060dd6353a8bce17c02041e8be09552c
dera <animicks.123.4@gmail.com>
2026-08-10 10:26:50 +0100

    update

--- app/contact_page_component/heading.tsx
+++ app/contact_page_component/heading.tsx
@@ -1,11 +1,11 @@
 const heading = () => {
   return (
     <div className="w-full">
-      <div className="w-full max-w-7xl pt-16 md:pt-20 text-balance  mx-auto flex flex-col justify-center text-center items-center gap-3">
+      <div className="w-full px-4 max-w-7xl pt-16 md:pt-20 text-balance  mx-auto flex flex-col justify-center text-center items-center gap-3">
         <p className="text-[16px] tracking-[1.6px] text-[#10A4EA] font-medium ">
           GET IN TOUCH
         </p>
-        <p className="text-4xl font-medium text-[#031F4F] w-[520px] ">
+        <p className="text-4xl font-medium text-[#031F4F]  ">
           Great Ideas Begin with Great Conversations.
         </p>
         <p className="text-[16px] font-semibold text-[#7B899A] ">
