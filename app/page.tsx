5752cefc1e71e6ebb22e2bd8f38ce6a191544c6c
Davis Isibor <davisisibor@gmail.com>
2026-08-17 20:41:39 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Hero from "./hompage_component/hero";
 import Services from "./hompage_component/services";
 import Work from "./hompage_component/work";
 import Projectvideo from "./hompage_component/projectvideo";
-import LogoZoom from "./hompage_component/logo-zoom";
 import Test from "./hompage_component/test";
 
 const page = () => {
