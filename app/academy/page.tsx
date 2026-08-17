c8791ac0a1cff775a2761b23b1d7d26f04090c6a
IJampi <theijampi@gmail.com>
2026-08-17 14:37:27 +0100

    update

--- app/academy/page.tsx
+++ app/academy/page.tsx
@@ -1,26 +1,19 @@
 import Nav from "@/app/academypage_component/nav";
 import Section from "@/app/academypage_component/section";
 import Section2 from "@/app/academypage_component/section2";
-import Section3Array from "@/app/academypage_component/section3Array";
-import Section4 from "@/app/academypage_component/section5Array";
 import Section5Array from "@/app/academypage_component/section4";
 import Section6 from "@/app/academypage_component/section6";
-import Footer from "@/app/academypage_component/footer";
 import StartProject from "@/app/academypage_component/startprojectacademy";
-import Faq from "@/app/academypage_component/faq"
+import Faq from "@/app/academypage_component/faq";
 const Academypage = () => {
   return (
     <div className=" bg-[#FFFFF0] ">
       <Section />
-
       <Section5Array />
       <Section2 />
-
-      
-
       <Section6 />
       <StartProject />
-     <Faq/>
+      <Faq />
     </div>
   );
 };
--- app/academypage_component/footer.tsx
+++ app/academypage_component/footer.tsx
@@ -56,74 +56,85 @@ const footerLinkGroups: FooterLinkGroup[] = [
 
 const Footer = () => {
   return (
-    <div className="   px-3  md:px-12  mx-auto w-full">
-      {/* <div className="flex  flex-col"> */}
-      <div className=" flex md:flex-row flex-col   md:justify-between gap-8  mx-auto py-10 md:py-12 border-b border-[#7B899A]   -[#7B899A] ">
-        <div className="flex w-full gap-3   flex-col  md:col-span-1 ">
-          <div>
-            <Image
-              src={adovlogo}
-              alt="adovlogo"
-              className="h-8 w-auto md:h-auto scale-70  md:scale-100"
-            />
-          </div>
-          <h1 className="text-[#7B899A] text-xs sm:text-base">
-            Creating experiences. Empowering growth.
-          </h1>
-        </div>
-        <div className=" gap-5 grid  md:grid-cols-2 ">
-          <div className="flex flex-col gap-2">
-            <Link
-              href={"/"}
-              className="text-[#7B899A] font-bold hover:text-gray-700 transition-colors text-sm sm:text-base"
-            >
-              COMPANY
-            </Link>
-            <div className="flex flex-col gap-1 text-xs sm:text-base">
-              <UnderlineLink href="/works">Works</UnderlineLink>
-
-              <UnderlineLink href="/academy">Academy</UnderlineLink>
-              <UnderlineLink href="/contact">Contact</UnderlineLink>
+    <div className=" px-3  md:px-12">
+      <div className="    mx-auto max-w-7xl  ">
+        <div className=" flex md:flex-row flex-col md:justify-between gap-8  mx-auto py-10 md:py-12 border-b border-[#7B899A]   -[#7B899A] ">
+          <div className="flex w-full gap-3   flex-col  md:col-span-1 ">
+            <div className="">
+              <Image
+                src={adovlogo}
+                alt="adovlogo"
+                className="h-8 w-auto md:h-auto scale-70  md:scale-100"
+              />
             </div>
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <Link
-              href={"/contact"}
-              className="text-[#7B899A] font-bold hover:text-gray-700 transition-colors text-sm sm:text-base"
-            >
-              CONTACT
-            </Link>
-            <div className="flex flex-col gap-1 text-xs sm:text-base break-words">
-              <UnderlineLink href="mailto:contactadov@gmail.com">
-                contact us
-              </UnderlineLink>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="  flex justify-between m-3 ">
-        <div className="flex gap-2 items-center ">
-          <Image src={footerlogo} alt="footerlogo" />
-          <h1 className="text-sm text-[#7B899A] sm:text-base">2026 Adov.</h1>
-        </div>
-        <div className="">
-          <Link
-            href="https://www.instagram.com/adov.official"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex gap- items-center hover:opacity-70 transition-opacity"
-          >
-            <Image src={insta} alt="instagram" />
-            <h1 className="text-[#7B899A] text-sm sm:text-base">
-              adov.official
+            <h1 className="text-[#7B899A] text-xs sm:text-base">
+              Creating experiences. Empowering growth.
             </h1>
-          </Link>
+          </div>
+          <div className="  flex gap-3 ">
+            <div className="flex flex-col  gap-2">
+              <Link
+                href={"/"}
+                className="text-[#7B899A] font-bold hover:text-gray-700 transition-colors text-sm sm:text-base"
+              >
+                COMPANY
+              </Link>
+              <div className="flex flex-col gap-1 text-xs sm:text-base">
+                <UnderlineLink href="/works">Works</UnderlineLink>
+
+                <UnderlineLink href="/academy">Academy</UnderlineLink>
+              </div>
+            </div>
+            <div className="flex flex-col  gap-2">
+              <Link
+                href={"/services"}
+                className="text-[#7B899A] font-bold hover:text-gray-700 transition-colors text-sm sm:text-base"
+              >
+                SERVICES
+              </Link>
+              <div className="flex flex-col gap-1 text-xs sm:text-base">
+                <UnderlineLink href="/works">Website</UnderlineLink>
+                <UnderlineLink href="/works">UI/UX</UnderlineLink>
+              </div>
+            </div>
+
+            <div className=" flex flex-col gap-2  ">
+              <Link
+                href={"/contact"}
+                className="text-[#7B899A] font-bold hover:text-gray-700 transition-colors text-sm sm:text-base"
+              >
+                CONTACT
+              </Link>
+              <div className="flex flex-col gap-1 text-xs sm:text-base break-words">
+                <UnderlineLink href="mailto:contactadov@gmail.com">
+                  contactadov@gmail.com
+                </UnderlineLink>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="  flex justify-between m-3 ">
+          <div className="flex gap-2 items-center ">
+            <Image src={footerlogo} alt="footerlogo" />
+            <h1 className="text-sm text-[#7B899A] sm:text-base">2026 Adov.</h1>
+          </div>
+          <div className="">
+            <Link
+              href="https://www.instagram.com/adov.official"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex gap- items-center hover:opacity-70 transition-opacity"
+            >
+              <Image src={insta} alt="instagram" />
+              <h1 className="text-[#7B899A] text-sm sm:text-base">
+                adov.official
+              </h1>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-    // </div>
   );
 };
 
